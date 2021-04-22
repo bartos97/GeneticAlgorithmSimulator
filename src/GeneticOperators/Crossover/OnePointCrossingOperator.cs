6fe349cb5da024d06dc2595f79052f0065fb6200
Bartłomiej Zając <bartoss97@gmail.com>
2021-04-22 19:20:20 +0200

    TwoPointCrossingOperator implementation

--- src/GeneticOperators/Crossover/OnePointCrossingOperator.cs
+++ src/GeneticOperators/Crossover/OnePointCrossingOperator.cs
@@ -12,22 +12,36 @@ namespace GeneticAlgorithmSimulator.GeneticOperators.Crossover
     {
         public void ApplyOn(Individual individual1, Individual individual2)
         {
+#if DEBUG
+            AssertChromosomes(individual1, individual2);
+#endif
             for (int i = 0; i < individual1.Chromosomes.Length; i++)
             {
-                Debug.Assert(individual1.Chromosomes[i].Builder.Length == individual2.Chromosomes[i].Builder.Length);
-                //Debug.Assert(individual1.GetHashCode() == individual2.GetHashCode());
-
                 int genesLength = individual1.Chromosomes[i].Builder.Length;
                 int middleIndex = genesLength / 2;
                 int cutoffLength = genesLength - middleIndex;
-                var sb1 = individual1.Chromosomes[i].Builder;
-                var sb2 = individual2.Chromosomes[i].Builder;
-                var copyInd1 = sb1.ToString(middleIndex, cutoffLength);
+                Swap(individual1.Chromosomes[i], individual2.Chromosomes[i], middleIndex, cutoffLength);
+            }
+        }
 
-                sb1.Remove(middleIndex, cutoffLength);
-                sb1.Insert(middleIndex, sb2.ToString(middleIndex, cutoffLength));
-                sb2.Remove(middleIndex, cutoffLength);
-                sb2.Insert(middleIndex, copyInd1);
+        public static void Swap(Chromosome chrom1, Chromosome chrom2, int startIndex, int cutoffLength)
+        {
+            var sb1 = chrom1.Builder;
+            var sb2 = chrom2.Builder;
+            var copyInd1 = sb1.ToString(startIndex, cutoffLength);
+
+            sb1.Remove(startIndex, cutoffLength);
+            sb1.Insert(startIndex, sb2.ToString(startIndex, cutoffLength));
+            sb2.Remove(startIndex, cutoffLength);
+            sb2.Insert(startIndex, copyInd1);
+        }
+
+        public static void AssertChromosomes(Individual individual1, Individual individual2)
+        {
+            Debug.Assert(individual1.Chromosomes.Length == individual2.Chromosomes.Length);
+            for (int i = 0; i < individual1.Chromosomes.Length; i++)
+            {
+                Debug.Assert(individual1.Chromosomes[i].Builder.Length == individual2.Chromosomes[i].Builder.Length);
             }
         }
     }
--- src/GeneticOperators/Crossover/TwoPointCrossingOperator.cs
+++ src/GeneticOperators/Crossover/TwoPointCrossingOperator.cs
@@ -1,6 +1,7 @@
 ﻿using GeneticAlgorithmSimulator.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,20 @@ namespace GeneticAlgorithmSimulator.GeneticOperators.Crossover
 {
     public class TwoPointCrossingOperator : IBinaryOperator
     {
+        private static readonly Random rand = new();
+
         public void ApplyOn(Individual individual1, Individual individual2)
         {
-            throw new NotImplementedException();
+#if DEBUG
+            OnePointCrossingOperator.AssertChromosomes(individual1, individual2);
+#endif
+            int maxCutoffLength = individual1.Chromosomes[0].Builder.Length - 4;
+            for (int i = 0; i < individual1.Chromosomes.Length; i++)
+            {
+                int cutoffLength = rand.Next(2, maxCutoffLength);
+                int cutoffStart = rand.Next(0, individual1.Chromosomes[i].Builder.Length - cutoffLength);
+                OnePointCrossingOperator.Swap(individual1.Chromosomes[i], individual2.Chromosomes[i], cutoffStart, cutoffLength);
+            }
         }
     }
 }
