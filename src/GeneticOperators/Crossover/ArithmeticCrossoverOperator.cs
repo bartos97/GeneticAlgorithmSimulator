5abe67adb8d02f5f971020f0b4d8b3f8c2194f63
Bartłomiej Zając <bartoss97@gmail.com>
2021-05-15 17:43:57 +0200

    Real chromosome crossover implementations

--- src/GeneticOperators/Crossover/ArithmeticCrossoverOperator.cs
+++ src/GeneticOperators/Crossover/ArithmeticCrossoverOperator.cs
@@ -9,9 +9,20 @@ namespace GeneticAlgorithmSimulator.GeneticOperators.Crossover
 {
     public class ArithmeticCrossoverOperator : IBinaryOperator
     {
+        private static readonly Random rand = new();
+
         public void ApplyOn(Individual individual1, Individual individual2)
         {
-            
+            double k = rand.NextDouble();
+            double x1 = individual1.Chromosomes[0];
+            double y1 = individual1.Chromosomes[1];
+            double x2 = individual2.Chromosomes[0];
+            double y2 = individual2.Chromosomes[1];
+
+            individual1.Chromosomes[0] = k * x1 + (1.0 - k) * x2;
+            individual1.Chromosomes[1] = k * y1 + (1.0 - k) * y2;
+            individual2.Chromosomes[0] = (1.0 - k) * x1 + k * x2;
+            individual2.Chromosomes[1] = (1.0 - k) * y1 + k * y2;
         }
     }
 }
--- src/GeneticOperators/Crossover/HeuristicCrossoverOperator.cs
+++ src/GeneticOperators/Crossover/HeuristicCrossoverOperator.cs
@@ -1,6 +1,7 @@
 ﻿using GeneticAlgorithmSimulator.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,50 @@ namespace GeneticAlgorithmSimulator.GeneticOperators.Crossover
 {
     public class HeuristicCrossoverOperator : IBinaryOperator
     {
+        private static readonly Random rand = new();
+
         public void ApplyOn(Individual individual1, Individual individual2)
         {
-            
+            double x1 = individual1.Chromosomes[0];
+            double y1 = individual1.Chromosomes[1];
+            double x2 = individual2.Chromosomes[0];
+            double y2 = individual2.Chromosomes[1];
+
+            if (x2 > x1 && y2 > y1)
+            {
+                DoCrossover(individual1, individual2);
+            }
+            else if (x2 < x1 && y2 < y1)
+            {
+                DoCrossover(individual2, individual1);
+            }
+            else if (y2 < y1)
+            {
+                individual2.Chromosomes[1] = y1;
+                individual1.Chromosomes[1] = y2;
+                DoCrossover(individual1, individual2);
+            }
+            else if (x2 < x1)
+            {
+                individual2.Chromosomes[0] = x1;
+                individual1.Chromosomes[0] = x2;
+                DoCrossover(individual1, individual2);
+            }
+            else Debug.Assert(false, "This should never happened");
+
+
+        }
+
+        private static void DoCrossover(Individual individual1, Individual individual2)
+        {
+            double k = rand.NextDouble();
+            double x1 = individual1.Chromosomes[0];
+            double y1 = individual1.Chromosomes[1];
+            double x2 = individual2.Chromosomes[0];
+            double y2 = individual2.Chromosomes[1];
+
+            individual1.Chromosomes[0] = k * (x2 - x1) + x1;
+            individual2.Chromosomes[1] = k * (y2 - y1) + y1;
         }
     }
 }
